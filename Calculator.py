4e2b23a4e1ac5eee82da55726aed5e389c6d8ab3
Rajagopal <rajagopalx@gmail.com>
2017-03-14 15:45:05 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,26 +1,26 @@
 def add(x,y):
-    print "ADDING %d + %d" % (x,y)
-    return x + y
+    print("ADDING %d + %d" % (x,y))
+    print(x + y)
 
 def subtract(x, y):
-    print "SUBTRACTING %d - %d" % (x, y)
+    print("SUBTRACTING %d - %d" % (x, y))
     return x - y
 
 def multiply(x, y):
-    print "MULTIPLYING %d * %d" % (x, y)
+    print("MULTIPLYING %d * %d" % (x, y))
     return x * y
 
 def divide(x, y):
-    print "DIVIDING %d / %d" % (x, y)
+    print("DIVIDING %d / %d" % (x, y))
     return x / y
 print("1= ADDITION")
 print("2= SUBTRACTION")
 print("3= MULTIPLICATION")
 print("4= DIVISION")
 print("5= EXIT")
-Option=raw_input("Choose what operation you want to do")
+Option=input("Choose what operation you want to do")
 print("Enter two numbers for processing:")
-a, b = raw_input(), raw_input()
+a, b = int(input()), int(input())
 if Option=='1':
     add(a,b)
 elif Option=='2':
