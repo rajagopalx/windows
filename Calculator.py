2e3ce9f3ff0cfce7a94cb4e230d2412837e1cc98
Rajagopal <rajagopalx@gmail.com>
2017-03-14 16:10:37 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,4 @@
+
 def add(x,y):
     print("ADDING %d + %d" % (x,y))
     print(x + y)
@@ -13,21 +14,28 @@ def multiply(x, y):
 def divide(x, y):
     print("DIVIDING %d / %d" % (x, y))
     return x / y
-print("1= ADDITION")
-print("2= SUBTRACTION")
-print("3= MULTIPLICATION")
-print("4= DIVISION")
-print("5= EXIT")
+def main():
+  
+  print("1= ADDITION")
+  print("2= SUBTRACTION")
+  print("3= MULTIPLICATION")
+  print("4= DIVISION")
+  print("5= EXIT")
 Option=input("Choose what operation you want to do")
 print("Enter two numbers for processing:")
 a, b = int(input()), int(input())
 if Option=='1':
-    add(a,b)
+    print(add(a,b))
 elif Option=='2':
-    subtract(a,b)
+    print(subtract(a,b))
 elif Option=='3':
-    multiply(a,b)
+    print(multiply(a,b))
 elif Option=='4':
-    divide(a,b)
+    print(divide(a,b))
 elif Option=='5':
     exit;
+choice=input("Do you want to repeat")
+if choice.lower()=='y' or 'yes':
+  main()
+else:
+  print("Bye!!!")
