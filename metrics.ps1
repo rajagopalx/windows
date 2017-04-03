ba1fe3707f21711c21866d10302bbc0b88a0cc98
Rajagopal <rajagopalx@gmail.com>
2017-04-03 11:15:33 +0530

    Add flag for local user

--- metrics.ps1
+++ metrics.ps1
@@ -1,12 +1,18 @@
 $JSON = ''
+$localusers = Get-WmiObject -Class Win32_UserAccount | foreach { $_.Caption }
 Get-WmiObject Win32_PerfFormattedData_PerfProc_Process `
     | Where-Object { $_.name -inotmatch '_total' } `
     | ForEach-Object {
+            $localuser = 0
             $id = $_.IDProcess
             $Process = Get-Process -Id $id -IncludeUserName
             $app = $Process.Product -replace '\s',''
             if (!$app) { $app = "None"}
-            $owner = $Process.UserName -replace '\s',''
+            $owner = $Process.UserName
+            if (  $localusers.Contains($owner)) { 
+                $localuser = 1
+            }
+            $owner = $owner -replace '\s',''
             if (!$owner) { $owner = "None"}
             $user = $owner.Split('\')[1]
             $name = $_.Name -replace '\s',''
@@ -18,6 +24,6 @@ Get-WmiObject Win32_PerfFormattedData_PerfProc_Process `
             $iowritebytes = ([math]::Round($_.IOWriteBytesPersec/1Mb,2))
             $ioreadops = $_.IOReadOperationsPersec
             $iowriteops = $_.IOWriteOperationsPersec
-            $JSON += "windows,computer=$computer,process=$name,user=$user,title=$app id=$id,cpu_proc_per=$cpu_processor_per,cpu_user_per=$cpu_user_per,ram=$ram,diskread=$ioreadbytes,diskwrite=$iowritebytes,readops=$ioreadops,writeops=$iowriteops `n"
+            $JSON += "windows,computer=$computer,process=$name,user=$user,title=$app id=$id,cpu_proc_per=$cpu_processor_per,cpu_user_per=$cpu_user_per,ram=$ram,diskread=$ioreadbytes,diskwrite=$iowritebytes,readops=$ioreadops,writeops=$iowriteops,localuser=$localuser `n"
     }
 Invoke-WebRequest -uri "http://192.168.30.200:8086/write?db=telegraf" -Method POST -Body  $JSON
